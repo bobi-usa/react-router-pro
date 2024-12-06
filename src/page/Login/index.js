07d9e7b6baf8c0e85f4047ca20cce9a9578a7a54
bobi-usa <bobi_usa@163.com>
2024-12-06 09:41:54 +0800

    路由导航跳转

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,5 +1,16 @@
+import { Link, useNavigate } from "react-router-dom"
+
 const Login = () => {
-  return <div>我是登录页</div>
+  const navigate = useNavigate()
+  return (
+    <div>
+      我是登录页
+      {/* 声明式的写法 */}
+      <Link to="/article">跳转到文章页</Link>
+      {/* 命令式的写法 */}
+      <button onClick={() => { navigate('/article') }}>跳转到文章页</button>
+    </div>
+  )
 }
 
 export default Login
