ace9d37f3c5dcd4670f16cc98ce36b5326a2ae17
bobi-usa <bobi_usa@163.com>
2024-12-06 10:41:33 +0800

    嵌套路由配置

---
+++ src/page/Layout/index.js
@@ -0,0 +1,16 @@
+import { Link, Outlet } from "react-router-dom"
+
+const Layout = () => {
+  return (
+    <div>
+      我是一级路由Layout组件
+
+      <Link to="/board">面板</Link>
+      <Link to="/about">关于</Link>
+      {/* 配置二级路由的出口 */}
+      <Outlet />
+    </div>
+  )
+}
+
+export default Layout
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
-import Login from '../page/Login'
-import Article from '../page/Article'
-import Post from '../page/Post'
+import Layout from '../page/Layout'
+import Board from '../page/Board'
+import About from '../page/About'
 import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path: '/login',
+    path: '/',
     // 注意: 这里写的是JSX, 并不是和Vue一样的组件名
-    element: <Login />,
-  },
-  {
-    path: '/article',
-    element: <Article />,
-  },
-  {
-    path: '/post/:id/:name',
-    element: <Post />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/board',
+        element: <Board />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ]
   },
 ])
 
